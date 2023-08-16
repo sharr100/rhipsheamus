871c6fa9305feaa2d2bd32791ce7e406079086b1
Shannon Keith Harris <sharr100@login1.isaac.utk.edu>
2023-08-16 13:46:50 -0400

    updated run.RivetTrain

--- jobscripts/scripts/run.RivetTrain.sh
+++ jobscripts/scripts/run.RivetTrain.sh
@@ -3,13 +3,11 @@
 #SBATCH -A ACF-UTK0019            # The project account to be charged
 #SBATCH --nodes=1                    # Number of nodes
 #SBATCH --ntasks-per-node=1         # cpus per node 
-## SBATCH --partition=condo-cnattras
-#SBATCH --partition=campus      
+#SBATCH --partition=condo-cnattras      
 #SBATCH --time=0-09:00:00             # Wall time (days-hh:mm:ss)
 #SBATCH --output=/lustre/isaac/scratch/sharr100/FIFO/pythia/output/Pythia.o%J
 #SBATCH --error=/lustre/isaac/scratch/sharr100/FIFO/pythia/error/Pythia.e%J	 
-## SBATCH --qos=condo
-#SBATCH --qos=campus
+#SBATCH --qos=condo
 
 ## OPTIONS:
 ## -A ACF-UTK0019
